ec39e0ba75c827f68817940129e2ee9908148c33
José Valim <jose.valim@plataformatec.com.br>
2017-08-23 12:12:11 +0200

    assert_same and assert_format with options

--- test/code_formatter_test.exs
+++ test/code_formatter_test.exs
@@ -5,63 +5,63 @@ defmodule CodeFormatterTest do
 
   describe "integer literals" do
     test "in decimal base" do
-      assert_format "0"
-      assert_format "100"
+      assert_same "0"
+      assert_same "100"
     end
 
     @tag :skip
     test "in decimal base with leading zeros" do
-      assert_format "007"
+      assert_same "007"
     end
 
     test "in binary base" do
-      assert_format "0b0"
-      assert_format "0b1"
-      assert_format "0b101"
+      assert_same "0b0"
+      assert_same "0b1"
+      assert_same "0b101"
     end
 
     @tag :skip
     test "in binary base with leading zeros" do
-      assert_format "0b01"
+      assert_same "0b01"
     end
 
     test "in octal base" do
-      assert_format "0o77"
-      assert_format "0o0"
+      assert_same "0o77"
+      assert_same "0o0"
     end
 
     @tag :skip
     test "in octal base with leading zeros" do
-      assert_format "0o01"
+      assert_same "0o01"
     end
 
     test "in hex base" do
-      assert_format "0x1"
+      assert_same "0x1"
       assert_format "0xabcdef", "0xABCDEF"
     end
 
     @tag :skip
     test "in hex base with leading zeros" do
-      assert_format "0x01"
+      assert_same "0x01"
     end
 
     test "as chars" do
-      assert_format "?a"
-      assert_format "?1"
-      assert_format "?è"
-      assert_format "??"
-      assert_format "?\\\\"
-      assert_format "?\\s"
+      assert_same "?a"
+      assert_same "?1"
+      assert_same "?è"
+      assert_same "??"
+      assert_same "?\\\\"
+      assert_same "?\\s"
     end
   end
 
   describe "string literals (double-quoted)" do
     test "without escapes" do
-      assert_format ~S["foo"]
+      assert_same ~S["foo"]
     end
 
     test "with escapes" do
-      assert_format ~S["f\a\b\ro"]
+      assert_same ~S["f\a\b\ro"]
     end
 
     test "converts literal new lines into escaped new lines" do
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,16 +1,16 @@
 ExUnit.start()
 
 defmodule CodeFormatter.Case do
-  defmacro assert_format(good) do
-    quote bind_quoted: [good: good] do
-      assert_format(good, good)
+  defmacro assert_same(good, opts \\ []) do
+    quote bind_quoted: [good: good, opts: opts] do
+      assert IO.iodata_to_binary(CodeFormatter.format(good, opts)) == good
     end
   end
 
-  defmacro assert_format(bad, good) do
-    quote bind_quoted: [bad: bad, good: good] do
-      assert IO.iodata_to_binary(CodeFormatter.format(bad)) == good
-      assert IO.iodata_to_binary(CodeFormatter.format(good)) == good
+  defmacro assert_format(bad, good, opts \\ []) do
+    quote bind_quoted: [bad: bad, good: good, opts: opts] do
+      assert IO.iodata_to_binary(CodeFormatter.format(bad, opts)) == good
+      assert IO.iodata_to_binary(CodeFormatter.format(good, opts)) == good
     end
   end
 end
